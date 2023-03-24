0daf5f6cb6426083acc92c07ea1cc5f9d277d75e
Viral-23 <73192398+Viral-23@users.noreply.github.com>
2023-03-24 13:50:59 -0400

    initial commit

---
+++ src/main/java/rucafe/project4/Coffee.java
@@ -0,0 +1,12 @@
+package rucafe.project4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coffee {
+    private int cupSize;
+    private List<String> addIns = new ArrayList<>();
+    public Coffee() {
+
+    }
+}
---
+++ src/main/java/rucafe/project4/Order.java
@@ -0,0 +1,8 @@
+package rucafe.project4;
+
+import java.util.ArrayList;
+
+public class Order {
+    private int orderNumber;
+    private ArrayList<Object> itemsInOrder = new ArrayList<>();
+}
