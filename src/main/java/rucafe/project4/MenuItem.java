4dc591642ea39fc39d280b570334ba1e1b625ba6
Viral-23 <73192398+Viral-23@users.noreply.github.com>
2023-03-28 18:49:37 -0400

    Added javadoc

--- src/main/java/rucafe/project4/MenuItem.java
+++ src/main/java/rucafe/project4/MenuItem.java
@@ -1,5 +1,14 @@
 package rucafe.project4;
 
+/**
+ * This is an abstract class which is the superclass of all menu items.
+ * @author Viral Patel, Rohan Patel
+ */
 public abstract class MenuItem {
+    /**
+     * Abstract method which calculates the price of a menu item. All MenuItem subclasses implement this class,
+     * each having their own price based on the item.
+     * @return double: returns the price of the item.
+     */
     public abstract double itemPrice(); //subclasses must implement this method
 }
