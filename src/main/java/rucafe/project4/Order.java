14e6a1665bc869a0e860bbe7ad431111e819c197
Viral-23 <73192398+Viral-23@users.noreply.github.com>
2023-03-31 16:00:31 -0400

    Edited order class

--- src/main/java/rucafe/project4/Order.java
+++ src/main/java/rucafe/project4/Order.java
@@ -2,7 +2,22 @@ package rucafe.project4;
 
 import java.util.ArrayList;
 
+/**
+ * This is a class that keeps track of a customers order and order number.
+ * @author Viral Patel, Rohan Patel
+ */
+
 public class Order {
     private int orderNumber;
     private ArrayList<Object> itemsInOrder = new ArrayList<>();
+
+    /**
+     * Constructor for the order object, which contains the order number and list of menu items in the order.
+     * @param orderNumber int: The order number of the current customer.
+     * @param itemsInOrder Object: The list of menu items (donut and coffee) in the order.
+     */
+    public Order(int orderNumber, ArrayList<Object> itemsInOrder) {
+        this.orderNumber = orderNumber++;
+        this.itemsInOrder = itemsInOrder;
+    }
 }
